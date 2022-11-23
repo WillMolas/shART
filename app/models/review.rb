2499cf69e4e72b09aaca06cd9b96edf22b570976
Guillem Molas <gmolas1@gmail.com>
2022-11-23 19:49:54 +0100

    finished reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :artwork
   belongs_to :user
 
-  validates :rating, presence: true, inclusion: { in: 1..5 }
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
 end
